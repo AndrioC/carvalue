79fde218c9d9bc5c9f5e640231347c74d72ff3b6
Andrio <andrio.rodrigo.silva@hotmail.com>
2022-08-06 11:37:12 -0300

    serializing the outgoing report response

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../guards/auth.guard';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/users.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
+@Serialize(ReportDto)
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
